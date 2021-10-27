e5121d2032fadb1a1f0fb41fa1384bf350a25bd7
Valerii <34238347+bvsBitry@users.noreply.github.com>
2021-10-27 15:19:57 +0300

    domains helper cities

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,6 +37,7 @@ class HomeController extends Controller
         $rates = $this->rateJob->getRatesByCity();
 
         dd(
+            cities(),
             $rates,
             $services,
             $advantages,

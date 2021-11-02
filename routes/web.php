1fe1e06d43eaf34dcdff00e613b2185c120884db
mcbeat <mcbeat436@gmail.com>
2021-11-02 22:22:22 +0200

    add XXX route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => parseLocale(),'where' => ['locale' => '[a-z]{2}'],'mid
 
     Auth::routes(['register' => false]);
 
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'main'])->name('main');
+    Route::get('/', [App\Http\Controllers\XXXHomeController::class, 'main'])->name('main');
     Route::get('advantage', [App\Http\Controllers\AdvantageController::class, 'index'])->name('advantage.index');
     Route::get('advantage/{slug}', [App\Http\Controllers\AdvantageController::class, 'show'])->name('advantage.show');
     Route::get('service', [App\Http\Controllers\ServiceController::class, 'index'])->name('service.index');

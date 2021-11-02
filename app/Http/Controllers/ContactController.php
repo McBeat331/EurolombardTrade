1c5d0b0fe80c309b28cc787c0f005bc83e6e16f6
Valerii <34238347+bvsBitry@users.noreply.github.com>
2021-11-02 18:04:03 +0200

    current city contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,11 +24,13 @@ class ContactController extends Controller
     public function show()
     {
         $settings = $this->settingService->getAll();
+        $currentCity = $this->rateJob->selectedCity();
 
 
 //        $phone = $this->settingService->getField('phone'); // or null
         dd(
-            $settings
+            $settings,
+            $currentCity
         );
 //        return view('');
     }

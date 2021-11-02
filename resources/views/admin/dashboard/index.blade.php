9790e52572a88cfb3890bcba40e737299fd26fc9
mcbeat <mcbeat436@gmail.com>
2021-11-02 22:11:16 +0200

    remove title block admin

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,19 +3,6 @@
 @section('content')
     <!-- row -->
     <div class="container-fluid">
-        <div class="row page-titles mx-0">
-            <div class="col-sm-6 p-md-0">
-                <div class="welcome-text">
-                    <h4>Главная панель</h4>
-                </div>
-            </div>
-            <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="javascript:void(0)">Гавная панель</a></li>
-                </ol>
-            </div>
-        </div>
-
         <div class="row">
             <div class="col-lg-3 col-sm-6">
                 <div class="card">

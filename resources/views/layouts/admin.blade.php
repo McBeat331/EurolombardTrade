157eb3e1272bc6d12043fa3a9d706b233d5a7a09
mcbeat <mcbeat436@gmail.com>
2021-11-06 19:33:46 +0200

    fix title admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{!! 'Еврообмен . Быстрый обмен валют' !!}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('adminAssets/js/app.js') }}" defer></script>
